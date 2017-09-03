62b1bc2a536ab960050249705483f44b691b844d
nameIsYong <1228359686@qq.com>
2017-09-03 20:23:33 +0800

    增加下载图片。

--- my_project/spiders/dmoz_spider.py
+++ my_project/spiders/dmoz_spider.py
@@ -2,16 +2,22 @@
 
 import scrapy
 import sys
+import urllib
+import os
+
 from my_project.items import MyProjectItem
 
-# sys.setdefaultencoding('utf-8')
+#写入文件
+def downPic(url):
+    fileName = url[-8:]
+    local = "down_images/"+fileName
+    if not os.path.exists(fileName):
+        urllib.urlretrieve(url, local)
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["http://www.mmjpg.com/"]
-    start_urls = [
-        "http://www.mmjpg.com/"
-    ]
+    start_urls = ["http://www.mmjpg.com/"]
 
     def parse(self, response):
         list = response.xpath("//div[@class='pic']/ul/li")
@@ -19,12 +25,10 @@ class DmozSpider(scrapy.Spider):
         # print list;
         # for index in range(len(list)):
         for note in list:
-            name = note.xpath("./a/img/@src").extract()
+            url = note.xpath("./a/img/@src").extract()
             title = note.xpath("./a/img/@alt").extract()
             item  = MyProjectItem();
-            item['name'] = name;
+            item['name'] = url;
             item['title'] = title;
-            # print  name;
-            # print title;
-            print '---------------->'
+            downPic(url[0]);
             yield  item;
