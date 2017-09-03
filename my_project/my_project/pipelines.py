1aa8cd5f2b947848c55004f42506ea00e7280f40
nameIsYong <1228359686@qq.com>
2017-09-03 19:29:54 +0800

    提交代码

---
+++ my_project/my_project/pipelines.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import json
+
+class MyProjectPipeline(object):
+
+    def __init__(self):
+        self.f = open('myjsonData.json','w')
+
+    def process_item(self, item, spider):
+        print '--------------------------------------line2--------------------'
+        print  item['name'];
+        print  item['title'];
+        content = json.dumps(dict(item),ensure_ascii=False)+",\n"
+        self.f.write(content.encode("utf-8"));
+        return item
+
+    def close_spider(self,spider):
+        self.f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
